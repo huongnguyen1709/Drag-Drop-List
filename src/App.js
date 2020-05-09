50bae7f030f8fe88da9d724bcbed9b170334f648
huongnguyen1709 <huongnguyen.nth1709@gmail.com>
2020-05-09 10:06:31 +0300

    styled-component, set up react-beautiful-dnd

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import initialData from './Data/initial-data'
 import Column from './Components/Column'
 import 'css-reset-and-normalize'
+import { DragDropContext } from 'react-beautiful-dnd'
 
 class App extends Component {
 
@@ -10,9 +11,18 @@ class App extends Component {
         this.state = initialData
     }
 
+    onDragEnd = result => {
+        // TODO: reorder our column, to change state
+    }
+
     render() {
         return (
-            <div>
+            // the only required callback for a DragDropContext is onDragEnd
+            <DragDropContext
+                // onDragStart // is called when the drag start
+                // onDragUpdate // is called when something changes during a drag such as an item is moving into a new position
+                onDragEnd={this.onDragEnd} // is called at the end of a drag
+            >
                 {
                     this.state.columnOrder.map(columnId => {
                         const column = this.state.columns[columnId]
@@ -22,7 +32,7 @@ class App extends Component {
                         return <Column key={columnId} column={column} tasks={tasks} />
                     })
                 }
-            </div>
+            </DragDropContext>
 
         );
     }
--- src/Components/Column.js
+++ src/Components/Column.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import styled from 'styled-components'
+import Task from './Task'
+import { Droppable } from 'react-beautiful-dnd'
+
+const Container = styled.div`
+    margin: 8px;
+    border: 1px solid lightgrey;
+    border-radius: 2px;
+`
+const Title = styled.h3`
+    padding: 8px;
+`
+const TaskList = styled.div`
+    padding: 8px;
+`
 
 const Column = (props) => {
     return (
         <div>
-            {props.column.title}
+            <Container>
+                <Title>{props.column.title}</Title>
+                <Droppable
+                // a Droppable has one required a droppableId
+                >
+                    <TaskList>
+                        {
+                            props.tasks.map(task => {
+                                return <Task key={task.id} content={task.content} />
+                            })
+                        }
+                    </TaskList>
+                </Droppable>
+            </Container>
         </div>
     );
 }
