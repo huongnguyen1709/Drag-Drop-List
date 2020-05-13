68b72ab49e2b8fc59c7c4bda02444f4ae6880dbb
huongnguyen1709 <huongnguyen.nth1709@gmail.com>
2020-05-13 11:32:51 +0300

    Updates

---
+++ precache-manifest.465b3311f17a29a2106b319dafbb801f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "531b65d2b34e4caec7df3b203d36d172",
+    "url": "/Drag-Drop-List/index.html"
+  },
+  {
+    "revision": "5f4bc6d8fc7f5f0a603b",
+    "url": "/Drag-Drop-List/static/css/2.331648e1.chunk.css"
+  },
+  {
+    "revision": "d3878b954eb69edddfec",
+    "url": "/Drag-Drop-List/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5f4bc6d8fc7f5f0a603b",
+    "url": "/Drag-Drop-List/static/js/2.e1f69ee8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Drag-Drop-List/static/js/2.e1f69ee8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3878b954eb69edddfec",
+    "url": "/Drag-Drop-List/static/js/main.358d1d48.chunk.js"
+  },
+  {
+    "revision": "b2250923e9d2f9c45f1a",
+    "url": "/Drag-Drop-List/static/js/runtime-main.650f1e0d.js"
+  }
+]);
