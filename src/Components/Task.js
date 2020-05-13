e93dd72b8ab312bcfdf1677affac8d5a6b176a47
huongnguyen1709 <huongnguyen.nth1709@gmail.com>
2020-05-13 20:58:01 +0300

    clean code

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -17,14 +17,6 @@ const Container = styled.div`
     };
 `
 
-// const Handle = styled.div`
-//     width: 20px;
-//     height: 20px;
-//     background-color: orange;
-//     border-radius: 4px;
-//     margin-right: 8px;
-// `
-
 const Task = (props) => {
     const isDragDisabled = props.task.id === 'task-1'
     return (
@@ -41,7 +33,6 @@ const Task = (props) => {
                     isDragging={snapshot.isDragging}
                     isDragDisabled={isDragDisabled}
                 >
-                    {/* <Handle {...provided.dragHandleProps} /> */}
                     {props.task.content}
                 </Container>
             )}
